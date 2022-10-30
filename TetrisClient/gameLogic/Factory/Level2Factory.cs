bc21e43141d489d59b6086fd10fa01eb6c7f2ee8
Kasparas Zakrys <k.zakrys@gmail.com>
2022-10-30 10:03:37 +0200

    builder2

--- TetrisClient/gameLogic/Factory/Level2Factory.cs
+++ TetrisClient/gameLogic/Factory/Level2Factory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TetrisClient.gameLogic.Bomb;
+using TetrisClient.gameLogic.Builder;
 using TetrisClient.gameLogic.Decorator;
 using TetrisClient.gameLogic.Factory;
 using TetrisClient.gameLogic.Tetromino;
@@ -17,22 +18,34 @@ namespace TetrisClient.gameLogic.Level
        
         public override Unit getTetromino(int offsetX, int offsetY, Random random)
         {
-            return new TetrominoLevel2(2, offsetX, offsetY, random, component);
+            //return new TetrominoLevel2(2, offsetX, offsetY, random, component);
+            Unit rawUnit = new TetrominoLevel2(2, offsetX, offsetY, random, component);
+            IBuilder builder = new BuildTetromino(rawUnit);
+            return this.director.getLevel(builder);
         }
 
         public override Unit getTetromino(int offsetX, int offsetY)
         {
-            return new TetrominoLevel2(2, offsetX, offsetY, component);
+            //return new TetrominoLevel2(2, offsetX, offsetY, component);
+            Unit rawUnit = new TetrominoLevel2(2, offsetX, offsetY, component);
+            IBuilder builder = new BuildTetromino(rawUnit);
+            return this.director.getLevel(builder);
         }
 
         public override Unit getTetromino(int offsetX, int offsetY, Matrix matrix)
         {
-            return new TetrominoLevel2(2, offsetX, offsetY, matrix, component);
+            //return new TetrominoLevel2(2, offsetX, offsetY, matrix, component);
+            Unit rawUnit = new TetrominoLevel2(2, offsetX, offsetY, matrix, component);
+            IBuilder builder = new BuildTetromino(rawUnit);
+            return this.director.getLevel(builder);
         }
 
         public override Unit getBomb()
         {
-            return new BombLevel2();
+            //return new BombLevel2();
+            Unit rawUnit = new BombLevel2();
+            IBuilder builder = new BuildBomb(rawUnit);
+            return this.director.getBomb(builder);
         }
     }
 }
